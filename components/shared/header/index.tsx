ace6f6640def25368821f909a6686c1585bb3152
apykwan <apykwan@hotmail.com>
2025-01-12 04:35:10 -0800

    add loading component, not-found page, and theme toggle

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
 
+import ModeToggle from './mode-toggle';
 import { APP_NAME } from '@/lib/constants';
 
 export default function Header() {
@@ -19,6 +20,8 @@ export default function Header() {
           </Link>
         </div>
         <div className="space-x-2">
+          <ModeToggle />
+
           <Button asChild variant="ghost">
             <Link href="/cart">
               <ShoppingCart /> Cart
