9ceaab21714791444b4bafd4fb7156b019856148
apykwan <apykwan@hotmail.com>
2025-01-13 15:09:13 -0800

    create seed function for dummy products

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,18 @@
 import ProductList from '@/components/shared/product/product-list';
-import sampleData from '@/db/sample-data';
+import { getLatestProducts } from '@/lib/actions/product.actions';
+import { LATEST_PRODUCTS_LIMIT } from '@/lib/constants';
+// import sampleData from '@/db/sample-data';
 
-export default function Homepage() {
-  console.log(sampleData);
+export default async function Homepage() {
+  const latestProducts = await getLatestProducts();
+  console.log(latestProducts);
   return (
     <>
-      <ProductList data={sampleData.products} title='Newest Arrival' limit={4} />
+      <ProductList 
+        data={latestProducts} 
+        title='Newest Arrival' 
+        limit={LATEST_PRODUCTS_LIMIT} 
+      />
     </>
   );
 }
