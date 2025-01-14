4b9ff6ecfa407f810ada6dfd3f186f7eee54bbd5
apykwan <apykwan@hotmail.com>
2025-01-14 03:04:24 -0800

    add zod to validate schema for typescript

--- components/shared/product/product-list.tsx
+++ components/shared/product/product-list.tsx
@@ -1,6 +1,7 @@
 import ProductCard from './product-card';
+import { Product } from '@/types';
 
-export default function ProductList({ data, title, limit }: { data: any; title?: string; limit?: number; }) {
+export default function ProductList({ data, title, limit }: { data: Product[]; title?: string; limit?: number; }) {
   const limitedData = limit ? data.slice(0, limit): data;
   
   return (
@@ -8,7 +9,7 @@ export default function ProductList({ data, title, limit }: { data: any; title?:
       <h2 className="h2-bold mb-4">{title}</h2>
       {limitedData.length > 0 ? (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          {limitedData.map((product: any) => (
+          {limitedData.map((product: Product) => (
             <ProductCard key={product.slug} product={product} />
           ))}
         </div>
