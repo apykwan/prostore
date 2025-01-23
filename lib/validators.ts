b69f1f888fb117b974109fef0779d83de40010af
apykwan <apykwan@hotmail.com>
2025-01-22 16:33:54 -0800

    create schema for cart

--- lib/validators.ts
+++ lib/validators.ts
@@ -40,3 +40,13 @@ export const signUpFormSchema = z.object({
   message: "password don't match",
   path: ["confirmPassword"]
 });
+
+// Cart Schemas
+export const cartItemSchema = z.object({
+  productId: z.string().min(1, 'Product is required'),
+  name: z.string().min(1, 'Name is required'),
+  slug: z.string().min(1, 'Slug is required'),
+  qty: z.number().int().nonnegative('Quantity must be a positive number'),
+  image: z.string().min(1, 'Image is required'),
+  price: currency,
+});
