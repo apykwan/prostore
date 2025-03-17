57437a3218c98835f32dd8f786431ae6b4902969
apykwan <apykwan@hotmail.com>
2025-03-17 02:43:00 -0700

    update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
-// import NextAuth from 'next-auth';
+import NextAuth from 'next-auth';
 
-// import { authConfig } from './auth.config';
+import { authConfig } from './auth.config';
 
-// export const { auth: middleware } = NextAuth(authConfig);
+export const { auth: middleware } = NextAuth(authConfig);
 
-export { auth as middleware } from '@/auth';
+// export { auth as middleware } from '@/auth';
